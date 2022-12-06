9e377bdfc3fe46c14c6df3e1e6cc04e21f13c12e
Sepehr Aghapour <dev.sepehr@outlook.com>
2022-12-06 17:22:54 +0330

    mobile menu completed

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let humbugger = document.querySelector(".humbugger");
+let rightMenu = document.querySelector(".right-menu");
+let rightNavItems = document.querySelectorAll(".right-nav");
+
+humbugger.addEventListener("click", () => {
+  console.log("clicked");
+  console.log(rightMenu);
+  rightMenu.classList.add("active");
+});
+
+document.onclick = (e) => {
+  if (!rightMenu.contains(e.target) && !humbugger.contains(e.target)) {
+    rightMenu.classList.remove("active");
+  }
+};
+
+for (let nav of rightNavItems) {
+  nav.onclick = () => {
+    rightMenu.classList.remove("active");
+  };
+}
